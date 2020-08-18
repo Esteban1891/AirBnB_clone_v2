25563708accda0a964ec79a71b72aefd41f0c933
Esteban1891 <esteban.delahoz15@gmail.com>
2020-08-17 22:35:18 -0500

    advance

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,75 +1,28 @@
-  
 #!/usr/bin/python3
-"""Distributes an archive to your web servers, using the function do_deploy"""
-from fabric.contrib import files
-from fabric.api import env, put, run, local
-import time
+# Fabfile to delete out-of-date archives.
 import os
+from fabric.api import *
 
-env.hosts = ['34.73.222.49', '54.82.121.70']
-
-
-def do_pack():
-    """Gerenate tgz."""
-    timestamp = time.strftime("%Y%m%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{:s}.tgz web_static/".
-              format(timestamp))
-        return ("versions/web_static_{:s}.tgz".format(timestamp))
-    except:
-        return None
-
-
-def do_deploy(archive_path):
-    """Function for deploy."""
-    if not os.path.exists(archive_path):
-        return False
-
-    data_path = '/data/web_static/releases/'
-    tmp = archive_path.split('.')[0]
-    name = tmp.split('/')[1]
-    dest = data_path + name
-
-    try:
-        put(archive_path, '/tmp')
-        run('mkdir -p {}'.format(dest))
-        run('tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
-        run('rm -f /tmp/{}.tgz'.format(name))
-        run('mv {}/web_static/* {}/'.format(dest, dest))
-        run('rm -rf {}/web_static'.format(dest))
-        run('rm -rf /data/web_static/current'.format(name))
-        run('ln -s {} /data/web_static/current'.format(dest))
-        return True
-    except:
-        return False
-
-
-def deploy():
-    """Compress and upload files to remote server."""
-    path = do_pack()
-    print(path)
-    if path is None:
-        return False
-
-    return do_deploy(path)
-
-
-deploy()
+env.hosts = ['35.231.232.13', '3.92.138.255']
 
 
 def do_clean(number=0):
-    if number == 0 or number == 1:
-        with cd.local('./versions/'):
-            local("ls -lv | rev | cut -f 1 | rev | \
-            head -n +1 | xargs -d '\n' rm -rf")
-        with cd('/data/web_static/releases/'):
-            run("sudo ls -lv | rev | cut -f 1 | \
-            rev | head -n +1 | xargs -d '\n' rm -rf")
-    else:
-        with cd.local('./versions/'):
-            local("ls -lv | rev | cut -f 1 | rev | \
-            head -n +{} | xargs -d '\n' rm -rf".format(number))
-        with cd('/data/web_static/releases/'):
-            run("sudo ls -lv | rev | cut -f 1 | \
-            rev | head -n +{} | xargs -d '\n' rm -rf".format(number))
+    """Delete out-of-date archives.
+    Args:
+        number (int): The number of archives to keep.
+    If number is 0 or 1, keeps only the most recent archive. If
+    number is 2, keeps the most and second-most recent archives,
+    etc.
+    """
+    number = 1 if int(number) == 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]

19e8b91a646b5a876618ff626368a966f9876544
Esteban1891 <esteban.delahoz15@gmail.com>
2020-08-17 22:29:20 -0500

    advance

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,56 +1,61 @@
-"""
-Fabric 100-clean_web_static.py file to web servers
-"""
-
-from fabric.api import env, local, put, run
-from datetime import datetime
-from os.path import exists, isdir
-
+  
+#!/usr/bin/python3
+"""Distributes an archive to your web servers, using the function do_deploy"""
+from fabric.contrib import files
+from fabric.api import env, put, run, local
+import time
+import os
 
 env.hosts = ['34.73.222.49', '54.82.121.70']
 
+
 def do_pack():
-    ts = time.strftime("%Y%m%d%H%M%S")
+    """Gerenate tgz."""
+    timestamp = time.strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{}.tgz web_static/".
-              format(ts))
-        return ("versions/web_static_{}.tgz".format(ts))
+        local("tar -cvzf versions/web_static_{:s}.tgz web_static/".
+              format(timestamp))
+        return ("versions/web_static_{:s}.tgz".format(timestamp))
     except:
         return None
 
 
 def do_deploy(archive_path):
-    """
-    deploy
-    """
-    if (os.path.isfile(archive_path) is False):
+    """Function for deploy."""
+    if not os.path.exists(archive_path):
         return False
 
+    data_path = '/data/web_static/releases/'
+    tmp = archive_path.split('.')[0]
+    name = tmp.split('/')[1]
+    dest = data_path + name
+
     try:
-        new_comp = archive_path.split("/")[-1]
-        new_folder = ("/data/web_static/releases/" + new_comp.split(".")[0])
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(new_folder))
-        run("sudo tar -xzf /tmp/{} -C {}".
-            format(new_comp, new_folder))
-        run("sudo rm /tmp/{}".format(new_comp))
-        run("sudo mv {}/web_static/* {}/".format(new_folder, new_folder))
-        run("sudo rm -rf {}/web_static".format(new_folder))
-        run('sudo rm -rf /data/web_static/current')
-        run("sudo ln -s {} /data/web_static/current".format(new_folder))
+        put(archive_path, '/tmp')
+        run('mkdir -p {}'.format(dest))
+        run('tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
+        run('rm -f /tmp/{}.tgz'.format(name))
+        run('mv {}/web_static/* {}/'.format(dest, dest))
+        run('rm -rf {}/web_static'.format(dest))
+        run('rm -rf /data/web_static/current'.format(name))
+        run('ln -s {} /data/web_static/current'.format(dest))
         return True
     except:
         return False
 
 
 def deploy():
-    try:
-        archive_address = do_pack()
-        val = do_deploy(archive_address)
-        return val
-    except:
+    """Compress and upload files to remote server."""
+    path = do_pack()
+    print(path)
+    if path is None:
         return False
+
+    return do_deploy(path)
+
+
+deploy()
 
 
 def do_clean(number=0):
